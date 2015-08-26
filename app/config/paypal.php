938fa526c1bf4b9672d4b12191755d372edbabba
Zurez <zurez4u@gmail.com>
2015-08-27 04:33:31 +0530

    Paypal and Product Page

---
+++ app/config/paypal.php
@@ -0,0 +1,39 @@
+<?php
+return array(
+    // set your paypal credential
+    'client_id' => 'Af9NApXRP2LNEcdSLzREszFLXVtAlq4LfLYVskU1nMQp2mKwq0Eij8vdWmkGRj18I9PC50ioSB8shhTe',
+    'secret' => 'EMC6ZJV3FdlXNM9goo1eEj8QlyN2vtlHfXFiJJlcyxvosEITFi4cHClwgn0qHpBc5XLlsVM9GWHvnsz2',
+
+    /**
+     * SDK configuration 
+     */
+    'settings' => array(
+        /**
+         * Available option 'sandbox' or 'live'
+         */
+        'mode' => 'sandbox',
+
+        /**
+         * Specify the max request time in seconds
+         */
+        'http.ConnectionTimeOut' => 30,
+
+        /**
+         * Whether want to log to a file
+         */
+        'log.LogEnabled' => true,
+
+        /**
+         * Specify the file that want to write on
+         */
+        'log.FileName' => storage_path() . '/logs/paypal.log',
+
+        /**
+         * Available option 'FINE', 'INFO', 'WARN' or 'ERROR'
+         *
+         * Logging is most verbose in the 'FINE' level and decreases as you
+         * proceed towards ERROR
+         */
+        'log.LogLevel' => 'FINE'
+    ),
+);
--- app/routes.php
+++ app/routes.php
@@ -4,6 +4,14 @@
 Route::get('/',function(){
 	return "Reached";
 });
-
+Route::group(array('before' => 'auth'), function(){
 Route::get('admin',array('as'=>'admin','uses'=>'AdminController@index'));
+
+});
 Route::get('product',array('as'=>'product','uses'=>'ProductController@index'));
+Route::get('buy/{id}/paypal',array('as' =>'buy' ,'uses'=>'PaypalController@index' ));
+
+Route::get('order/status', array(
+    'as' => 'payment.status',
+    'uses' => 'IndexController@getPaymentStatus',
+));
